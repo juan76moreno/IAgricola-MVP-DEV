3bd3fd84742c81acdc7b6658572f9fd2859bc352
juan76moreno <juanalberto.moreno@bbva.com>
2026-07-07 15:05:25 -0400

    Migrar JavaScript a app.js

--- js/app.js
+++ js/app.js
@@ -1,1 +1,21 @@
+function preparar(){
 
+  document.getElementById('card').style.display='none';
+
+  document.getElementById('prep').style.display='block';
+
+  setTimeout(function(){
+
+    document.getElementById('last').innerText='✓';
+
+  },1000);
+
+  setTimeout(function(){
+
+    document.getElementById('prep').style.display='none';
+
+    document.getElementById('ready').style.display='block';
+
+  },2200);
+
+}
