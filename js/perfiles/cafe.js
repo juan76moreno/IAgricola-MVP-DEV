f99f373a797ecf3b3fdd562d0fc2c5a24d01dd29
juan76moreno <juanalberto.moreno@bbva.com>
2026-08-13 09:18:00 -0400

    Configurar discriminadores de Café en motor multirrubro

--- js/perfiles/cafe.js
+++ js/perfiles/cafe.js
@@ -31,3 +31,45 @@ nombre: "Cafe",
     };
 
 }
+nombre: "Café",
+
+    discriminadores: [
+
+        {
+            campo: "variedadCafe",
+            origenExistente: "variedadCafe"
+        },
+
+        {
+            campo: "edadCafe",
+            origenExistente: "edadCafe"
+        },
+
+        {
+            campo: "densidadCafe",
+            origenExistente: "densidadCafe"
+        },
+
+        {
+            campo: "estadoProductivoCafe",
+            origenExistente: "estadoProductivoCafe"
+        },
+
+        {
+            campo: "sombraCafe",
+            origenExistente: "sombraCafe"
+        },
+
+        {
+            campo: "rendimientoCafe",
+            origenExistente: "rendimientoCafe"
+        },
+
+        {
+            campo: "estadoFitosanitarioCafe",
+            origenExistente: "estadoFitosanitarioCafe"
+        }
+
+    ]
+
+});
